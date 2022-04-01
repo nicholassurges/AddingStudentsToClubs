f09fa86ba221cefaacb1b1b5c00e75c5b0de0f0d
nicholassurges <81642890+nicholassurges@users.noreply.github.com>
2022-03-31 21:29:45 -0400

    Add files via upload

---
+++ Adding Students to Clubs/Form1.Designer.vb
@@ -0,0 +1,187 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.cboClubs = New System.Windows.Forms.ComboBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.lblCount = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lstMembers = New System.Windows.Forms.ListBox()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnRemove = New System.Windows.Forms.Button()
+        Me.lstGeneral = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'cboClubs
+        '
+        Me.cboClubs.FormattingEnabled = True
+        Me.cboClubs.Location = New System.Drawing.Point(227, 21)
+        Me.cboClubs.Name = "cboClubs"
+        Me.cboClubs.Size = New System.Drawing.Size(164, 24)
+        Me.cboClubs.TabIndex = 0
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.Panel1.Controls.Add(Me.btnExit)
+        Me.Panel1.Controls.Add(Me.lblCount)
+        Me.Panel1.Controls.Add(Me.Label4)
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.lstMembers)
+        Me.Panel1.Controls.Add(Me.btnAdd)
+        Me.Panel1.Controls.Add(Me.btnRemove)
+        Me.Panel1.Controls.Add(Me.lstGeneral)
+        Me.Panel1.Controls.Add(Me.cboClubs)
+        Me.Panel1.Location = New System.Drawing.Point(27, 71)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(683, 380)
+        Me.Panel1.TabIndex = 1
+        '
+        'lblCount
+        '
+        Me.lblCount.AutoSize = True
+        Me.lblCount.Location = New System.Drawing.Point(580, 307)
+        Me.lblCount.Name = "lblCount"
+        Me.lblCount.Size = New System.Drawing.Size(88, 17)
+        Me.lblCount.TabIndex = 10
+        Me.lblCount.Text = "(0 Members)"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(370, 71)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(163, 17)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Club Membership List"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(28, 71)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(158, 17)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "General Student List"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(26, 24)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(154, 17)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Select the current club:"
+        '
+        'lstMembers
+        '
+        Me.lstMembers.FormattingEnabled = True
+        Me.lstMembers.ItemHeight = 16
+        Me.lstMembers.Location = New System.Drawing.Point(373, 100)
+        Me.lstMembers.Name = "lstMembers"
+        Me.lstMembers.Size = New System.Drawing.Size(182, 244)
+        Me.lstMembers.TabIndex = 6
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(227, 180)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(97, 93)
+        Me.btnAdd.TabIndex = 5
+        Me.btnAdd.Text = "Add Selected Students"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'btnRemove
+        '
+        Me.btnRemove.Location = New System.Drawing.Point(571, 180)
+        Me.btnRemove.Name = "btnRemove"
+        Me.btnRemove.Size = New System.Drawing.Size(97, 93)
+        Me.btnRemove.TabIndex = 4
+        Me.btnRemove.Text = "Remove Selected Students"
+        Me.btnRemove.UseVisualStyleBackColor = True
+        '
+        'lstGeneral
+        '
+        Me.lstGeneral.FormattingEnabled = True
+        Me.lstGeneral.ItemHeight = 16
+        Me.lstGeneral.Location = New System.Drawing.Point(31, 100)
+        Me.lstGeneral.Name = "lstGeneral"
+        Me.lstGeneral.Size = New System.Drawing.Size(182, 244)
+        Me.lstGeneral.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(24, 21)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(686, 34)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = resources.GetString("Label1.Text")
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(571, 21)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(97, 50)
+        Me.btnExit.TabIndex = 11
+        Me.btnExit.Text = "Close Program"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(734, 472)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents cboClubs As ComboBox
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents btnRemove As Button
+    Friend WithEvents lstGeneral As ListBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lblCount As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents lstMembers As ListBox
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnExit As Button
+End Class
---
+++ Adding Students to Clubs/Form1.vb
@@ -0,0 +1,137 @@
+﻿Public Class Form1
+    'Declare variables
+    'Adding the options in the combo box.
+    Private strClubNames() As String = {
+            "Honors", "Golden Arrows", "Computers", "Drama"}
+
+    Private studentNames() As String = {
+            "Adams, Ben", "Baker, Sally", "Canseco, Juan", "Davis, Sharon",
+            "Etienne, Jean", "Gonzalex, Jose", "Johnson, Eric", "Koenig, Joann",
+            "Matsunaga, Akiko", "Nakamura, Ken", "Ramirez, Mana", "Nick Surges"}
+
+    'Keep track of the size of each club
+    Private intEnrollSizes(strClubNames.Count) As Integer
+
+    'Holds names of enrolled members of all clubs
+    Private strEnrollments(0, 0) As String
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Add the clubNames to the combo box
+        For Each clubName In strClubNames
+            cboClubs.Items.Add(clubName)
+        Next
+        'add the student names to the student list box
+        For Each studName In studentNames
+            lstGeneral.Items.Add(studName)
+        Next
+
+        'Initiazlize the Enrollment array
+        ReDim strEnrollments(strClubNames.Length, lstGeneral.Items.Count)
+
+    End Sub
+
+    'Function for checking for errors
+    Function CheckForInsertErrors() As Boolean
+
+        'check for the combo box selection
+        If cboClubs.SelectedIndex = -1 Then
+            MessageBox.Show("Select the name of the club")
+            Return False
+        End If
+
+        'Check the selected index of lstGeneral
+        If lstGeneral.SelectedIndex = -1 Then
+            MessageBox.Show("Select the student from the studen list
+                to add to the club")
+            Return False
+        End If
+
+        'return true if there are no errors
+        Return True
+
+    End Function
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        'Add button event to add the student into the club
+
+        'Chek for errors
+        If Not CheckForInsertErrors() Then
+            Return
+        End If
+
+        'get the index of the club
+        Dim clubIndex As Integer = cboClubs.SelectedIndex
+
+        'get the index of the student names
+        Dim name As String = lstGeneral.SelectedItem.ToString()
+
+        'check to see if the student does not exist in the group
+        'then add the student to the list membership intEnroll array
+        If Not lstMembers.Items.Contains(name) Then
+            lstMembers.Items.Add(lstGeneral.SelectedItem)
+
+            intEnrollSizes(clubIndex) += 1
+            lblCount.Text = lstMembers.Items.Count.ToString() & " Members"
+        End If
+
+        'Save the names back inot the master array
+        For i = 0 To lstMembers.Items.Count - 1
+            strEnrollments(clubIndex, i) = lstMembers.Items(i).ToString()
+        Next
+
+    End Sub
+
+    Private Sub btnRemove_Click(sender As Object, e As EventArgs) Handles btnRemove.Click
+        'remove button event to remove students from the club member list
+
+        'check selected index
+        If lstMembers.SelectedIndex = -1 Then
+            Return
+        End If
+
+        'get the selected index
+        Dim index As Integer = lstMembers.SelectedIndex
+
+        'get the index of the club
+        Dim clubIndex As Integer = cboClubs.SelectedIndex
+
+        'if index is not -1 then remove the item from the
+        If index <> -1 Then
+            lstMembers.Items.RemoveAt(index)
+        End If
+
+        'Save the names back into the Master array
+        For i = 0 To lstMembers.Items.Count - 1
+            strEnrollments(clubIndex, i) = lstMembers.Items(i).ToString()
+        Next
+        'update the count of the intEnrollSixes
+        intEnrollSizes(clubIndex) = lstMembers.Items.Count
+
+        'Display the totatl number of members
+        lblCount.Text = lstMembers.Items.Count.ToString() & " Members"
+
+    End Sub
+
+    Private Sub cboClubs_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cboClubs.SelectedIndexChanged
+        'combo box selected index event
+
+        'Fill the member list box with the names of pople
+        'who belong to the currenlty selected club
+        lstMembers.Items.Clear()
+
+        Dim index As Integer = cboClubs.SelectedIndex
+
+        'Copy member names from the enrollments master
+        'array into the list box
+        For i As Integer = 0 To intEnrollSizes(index) - 1
+            lstMembers.Items.Add(strEnrollments(index, i))
+        Next
+
+        lblCount.Text = intEnrollSizes(index).ToString() & " Members"
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
